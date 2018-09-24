8058b530f7d2be3f62778ef5a873b5129d9e90a4
SergioPerez <dark07perez@gmail.com>
2018-09-24 01:23:37 -0500

    login screen

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -10,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using MySql.Data.MySqlClient;
 
 namespace Deportes_WPF
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Lógica de interacción para Login.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Login : Window
     {
-        public MainWindow()
+        public Login()
         {
             InitializeComponent();
-        }      
-       
+        }
     }
 }
