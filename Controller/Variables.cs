52f406cd026e33c38cfb0d357f0bbc909b2b57fd
SergioPerez <dark07perez@gmail.com>
2018-09-30 19:34:02 -0500

    periodo

---
+++ Controller/Variables.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deportes_WPF.Controller
+{
+    static class Variables
+    {
+
+         
+
+
+    }
+}
