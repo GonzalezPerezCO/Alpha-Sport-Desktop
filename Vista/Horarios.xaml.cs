8b1a76d5804f44f4d178cab2662d86f78d282b9c
SergioPerez <dark07perez@gmail.com>
2018-10-28 22:34:10 -0500

    correcion hide()

--- Vista/Horarios.xaml.cs
+++ Vista/Horarios.xaml.cs
@@ -43,8 +43,9 @@ namespace Deportes_WPF.Vista
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Window tabla = new TablaInscritos();
-            this.Hide();
+            
             tabla.Show();
+            this.Hide();
         }
 
         private void bt3_Click(object sender, RoutedEventArgs e)
