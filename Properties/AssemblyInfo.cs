4dc7772789e560bdc6641e06f722e2d6b90a77e3
SergioPerez-e <dark07perez@gmail.com>
2019-01-02 01:04:46 -0500

    solucion System.Windows.Baml2006

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("AlphaSport")]
-[assembly: AssemblyDescription("SISTEMA DE GESTIÓN DE RECURSOS DEPORTIVOS DE LA ESCUELA")]
+[assembly: AssemblyTitle("Deportes-WPF")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GonzalezPerezCO")]
-[assembly: AssemblyProduct("SISTEMA DE GESTIÓN DE RECURSOS DEPORTIVOS")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("GonzalezPerezCO")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Deportes-WPF")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
