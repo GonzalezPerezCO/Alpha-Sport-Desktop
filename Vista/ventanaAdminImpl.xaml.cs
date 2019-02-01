e480b205ae47b912217f031d85db51c6075e591b
SergioPerez-e <dark07perez@gmail.com>
2019-02-01 04:25:08 -0500

    agregando ventana admin impl

--- Vista/ventanaAdminImpl.xaml.cs
+++ Vista/ventanaAdminImpl.xaml.cs
@@ -23,6 +23,8 @@ namespace AlphaSport.Vista
         private Entorno entorno;
         private static VentanaAdminImpl instance = null;
 
+        private bool nuevoImpl; // si es o no un nuevo prestamo
+
         public VentanaAdminImpl()
         {
             InitializeComponent();
@@ -44,20 +46,32 @@ namespace AlphaSport.Vista
         private void Limpiar()
         {
             text1.Text = "";
+
+            nuevoImpl = false;
+
             cmbox1.SelectedValue = null;
 
             text1.Focus();
         }
 
-        private void ActualizarCmbx() {
-        }
-
-        private void Btn1_Click(object sender, RoutedEventArgs e)
+        private void ActualizarCmbx()
         {
 
         }
 
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+            Ocultar();
+        }
+
         private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+            Limpiar();
+            Ocultar();
+        }
+
+        private void Ocultar()
         {
             TablaImplementos impl = TablaImplementos.GetInstance();
             impl.Show();
